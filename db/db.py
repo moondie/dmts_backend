3063da8490e63f95ece79fda925420cc904804fc
liwenke1 <949484025@qq.com>
2023-07-16 03:32:14 +0800

    fix: update variable scope
    change protected to private

--- db/db.py
+++ db/db.py
@@ -9,17 +9,17 @@ class MongoDB():
 
     def __init__(self, url="mongodb://root:LIwenke020@localhost:27017/") -> None:
         """根据 url 连接数据库和所需要的表"""
-        self._db = pymongo.MongoClient(url)["dmts"]
-        self._t_tasks = self._db["tasks"]
-        self._t_repo_vector = self._db["repo_vector"]
-        self._t_analysis_results = self._db["analysis_results"]
-        self._logger = Log().logger
+        self.__db = pymongo.MongoClient(url)["dmts"]
+        self.__t_tasks = self.__db["tasks"]
+        self.__t_repo_vector = self.__db["repo_vector"]
+        self.__t_analysis_results = self.__db["analysis_results"]
+        self.__logger = Log().logger
 
     def get_task_list(self) -> list:
         """获取任务列表"""
-        self._logger.info("数据库读取任务列表")
+        self.__logger.info("数据库读取任务列表")
         task_list = []
-        for document in self._t_tasks.find():
+        for document in self.__t_tasks.find():
             if not document["is_effective"]:
                 continue
 
@@ -37,5 +37,5 @@ class MongoDB():
                 "size": document["size"],
             })
 
-        self._logger.info("数据库结束获取任务列表")
+        self.__logger.info("数据库结束获取任务列表")
         return task_list
